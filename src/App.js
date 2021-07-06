1d4c9d488af1927a9233dd1f0cc548a25e8d0772
Mrityunjay_Singh <singhmrityunjay32@gmail.com>
2021-07-06 09:46:11 +0530

    innitial-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import {  Card } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
+
+  const [pokemon, setPokemons] = useState([]);
+
+  const getAll = async () => {
+    const response = await fetch("https://dev-util.edyst.com/challenge/pokemons/random")
+    const data = await response.json();
+    console.log(data)
+    setPokemons(data);
+  }
+
+  useEffect(() => {
+    getAll();
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        {pokemon.map((item,idx) => {
+          return (
+            
+            <Card style={{ width: '18rem', backgroundColor: (item.cardColors.bg)}} className="card" key={idx}>
+              <Card.Body>
+              <div className="cont">
+                <Card.Text className="tagText" style={{ backgroundColor: (item.cardColors.tagbg)}}>{item.tag}
+                </Card.Text>
+                <FontAwesomeIcon icon={faArrowRight} />
+              </div>
+                
+                <Card.Text className="text" style={{ backgroundColor: (item.cardColors.textbg)}}><Card.Title style={{ fontFamily:"Arvo"}}>{item.name}</Card.Title>
+                  {item.description}
+                </Card.Text>
+              </Card.Body>
+              <Card.Img variant="top" src={`${item.sprite}`} className="imgrounded" style={{ backgroundColor: (item.cardColors.imgbg)}} />
+            </Card>
+
+          )
+        })}
+      </div>
     </div>
   );
 }
